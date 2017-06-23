ec42a048b889936418e1d3b668410f7c60194cda
farque65 <fahim.farque@gmail.com>
2017-06-23 10:53:55 -0400

    adding import statement and import function

--- src/plugins/ecs/__init__.py
+++ src/plugins/ecs/__init__.py
@@ -10,7 +10,7 @@ import sys
 from datetime import *
 import common
 import compare_output
-from .ecs_compares import main_ecs_check_versions
+import ecs_compares
 
 
 def main(text):
@@ -198,7 +198,7 @@ def main(text):
 															  config["General"]["build_link"],
 															  config["General"]["my_build_key"])
 
-					compared_data = main_ecs_check_versions(master_data,
+					compared_data = ecs_compares.main_ecs_check_versions(master_data,
 															team_data,
 															config["General"]["jenkins"]["branch_equivalent_tags"],
 															superjenkins_data,

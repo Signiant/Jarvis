65dd1d28f2426297ad180416372f326ec89cb53c
farque65 <fahim.farque@gmail.com>
2017-06-26 11:32:27 -0400

    append path of the current subdirectory module to sys.path

--- src/plugins/ping/__init__.py
+++ src/plugins/ping/__init__.py
@@ -1,3 +1,7 @@
+#append path of the current subdirectory module to sys.path so any modules in the current directory will load
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path)
+
 def main(*args):
   return "pong"
 

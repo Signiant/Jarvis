5c5048e9dcee086f61093d3b3917ff8c8ccd0533
William Jia <wjia@WJia-mac-3.ott.signiant.com>
2019-05-27 14:18:53 -0400

    change branch to does not match master

--- src/plugins/ecs/ecs_compares/__init__.py
+++ src/plugins/ecs/ecs_compares/__init__.py
@@ -116,10 +116,10 @@ def compare_environment(team_env, master_env, jenkin_build_terms):
                 result = 1
             else:
                 # if both version on bitbucket but not same commit hash
-                result = 3
+                result = 2
         else:
             # if one is jenkin build number and other one is bitbucket hash
-            result = 3
+            result = 2
 
     print("result",result)
     logging.debug("Bitbucket comparing %s and %s result is %s" % (team_env, master_env, result))
